fdb8eee5aca0d7f8fe6db74682f2703832a6d922
Feliphe Allef <felipheallef@gmail.com>
2022-08-15 14:05:16 -0300

    Updated API documentation

--- lib/app_icon_shape.dart
+++ lib/app_icon_shape.dart
@@ -3,10 +3,13 @@ import 'dart:ui';
 import 'app_icon_shape_platform_interface.dart';
 
 class AppIconShape {
+  /// Returns the platform's name and version
+  /// e.g. Android 12
   static Future<String?> getPlatformVersion() {
     return AppIconShapePlatform.instance.getPlatformVersion();
   }
 
+  /// On Android, returns
   static Future<Path?> getAppIconMask() {
     return AppIconShapePlatform.instance.getAppIconMask();
   }
